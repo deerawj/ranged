74199d27262727ed4249ca22d50e8fb0a88e468d
deerawj <deerawj@gmail.com>
2022-12-27 14:56:22 +0530

    added more tests

--- test/main.py
+++ test/main.py
@@ -6,6 +6,10 @@ MAX = 10*6
 def test_01():
     assert list(Range(MIN, MAX)) == list(range(MIN, MAX))
 
-def test_02():
+def test_func():
     assert ([i*2 for i in range(MIN, MAX)] == 
             list(Range(MIN, MAX, func=lambda x:x*2)))
+
+def test_filt():
+    assert ([i for i in range(MIN, MAX) if i%2 == 0] == 
+            list(Range(MIN, MAX, filt=lambda x:x%2 == 0)))
